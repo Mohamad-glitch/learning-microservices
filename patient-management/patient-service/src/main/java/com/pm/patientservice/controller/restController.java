d9981e148a4496048fba5e1c669bba0370c1c892
Mohamad-glitch <mohammad.altalib37@gmail.com>
2025-06-20 12:35:02 +0300

    added the gateway to access the microservices

--- patient-management/patient-service/src/main/java/com/pm/patientservice/controller/restController.java
+++ patient-management/patient-service/src/main/java/com/pm/patientservice/controller/restController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
-@RequestMapping("/patient")
+@RequestMapping("/patients")
 @RestController
 @Tag(name = "patient", description = "patient CRUD operation")
 public class restController {
